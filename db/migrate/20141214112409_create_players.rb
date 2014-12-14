123c4104e1b9492fb41cedf9e33011c8e906d339
OttlikG <ottlik.gab@gmail.com>
2014-12-14 17:45:48 +0100

    Added players CRUD

---
+++ db/migrate/20141214112409_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :email
+      t.integer :cash
+
+      t.timestamps
+    end
+  end
+end
+
